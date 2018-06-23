34bece0dc69da89a033791fbda747ab1027829a0
Vladimir <vladimirkitanovski22@gmail.com>
2018-06-23 17:43:45 +0200

    Add scroll reveal

--- source/js/app.js
+++ source/js/app.js
@@ -12,9 +12,43 @@ function showNav() {
 // JavaScript
 window.sr = ScrollReveal(); 
 
-// sr.reveal('h1', {
-//     delay: 0,
-//     duration: 200,
-//     origin: 'bottom',
-//     distance: '100px' 
-// });
+sr.reveal('#header img.phone', {
+    duration: 800,
+    origin: "bottom",
+    distance: "100px"
+});
+
+sr.reveal('#features div.car', {
+    delay: 200,
+    duration: 800,
+    origin: "bottom",
+    distance: "100px"
+});
+
+sr.reveal('#features div.shoe', {
+    delay: 400,
+    duration: 800,
+    origin: "bottom",
+    distance: "100px"
+});
+
+sr.reveal('#features div.device', {
+    delay: 600,
+    duration: 800,
+    origin: "bottom",
+    distance: "100px"
+});
+
+sr.reveal('#discount .offer', {
+    delay: 400,
+    duration: 800,
+    origin: "left",
+    distance: "300px"
+});
+
+sr.reveal('#discount .discountLayout', {
+    delay: 400,
+    duration: 800,
+    origin: "right",
+    distance: "300px"
+});
